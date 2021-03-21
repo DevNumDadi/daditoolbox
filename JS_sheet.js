34aea211237017b9feddbb45e65b3befe77b0554
NNooeeFF <58855987+NNooeeFF@users.noreply.github.com>
2021-03-21 17:47:29 +0100

    Create JS_sheet.js

---
+++ JS_sheet.js
@@ -0,0 +1,106 @@
+
+
+fetch('https://spreadsheets.google.com/feeds/cells/1vZHQRGxhb8Bukf3iqK4yTrBLtQf3VVDYdLrWYGDYGCU/1/public/full?alt=json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        saveData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+
+let dataContainer = [];
+let columnHeads = ["col1","col2","col3"]
+
+let nbLines = 13;
+//let nbCols = 3;
+
+
+function saveData(data) {
+
+  let nbLines = data.feed.entry.length / columnHeads.length
+
+    for (var i = 0; i < nbLines; i++) {
+        let key = "line" + i
+        dataContainer.push([]);
+    }
+
+    for (var i = 0; i < data.feed.entry.length; i++) {
+
+        let col = data.feed.entry[i].gs$cell.col
+        let row = data.feed.entry[i].gs$cell.row-1
+        let value = data.feed.entry[i].content.$t
+
+        dataContainer[row].push(value);
+    }
+}
+
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+
+function generateTable(table, data) {
+    
+var sel = document.getElementById('Disciplines');
+let sel_discipline = sel.value;
+console.log(sel_discipline);  
+
+    for (let element of data) {
+    let row = table.insertRow();
+
+    
+    for (var i = 0; i < element.length; i++) {
+
+        for (var n = 0; n < catTable.length; n++){
+            
+            if (element[0] == catTable[n] && element[1] == sel_discipline){
+                let cell = row.insertCell();
+                let text = document.createTextNode(element[i]);
+                cell.appendChild(text)
+            }
+        }
+        
+        }
+    }
+}
+
+
+function onClick() {
+
+    let table = document.querySelector("table");
+  
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+      }
+
+    generateTable(table, dataContainer);
+    generateTableHead(table, columnHeads);
+}
+
+
+let catTable = []
+
+function filterCol(n, cat){
+    const cb = document.getElementById('Check'+ n);
+    if  (cb.checked){
+        catTable.push(cat);        
+    }
+    else {
+        catTable.splice(catTable.indexOf(cat), 1);
+    }
+
+
+    onClick();
+    }
